919849731d88b3cad672b64109956912abec5abe
Erika <aversa.erika@gmail.com>
2025-06-08 12:11:18 +0200

    Fixed broken test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:run_with_me/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const RunWithMeApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
